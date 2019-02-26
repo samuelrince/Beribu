06d8c4218581de6b0d930edc101d3105af6ccffd
Samuel Rincé <samuel.rince.sr@gmail.com>
2019-02-26 23:16:02 +0100

    Change Model + Add Localization class

---
+++ src/fr/ecp/IS1220/beribu/Localization.java
@@ -0,0 +1,67 @@
+package fr.ecp.IS1220.beribu;
+
+public class Localization {
+	private double latitude;
+	private double longitude;
+	
+	public Localization(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	@Override
+	public String toString() {
+		return "GPS (" + latitude + ", " + longitude + ")";
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(latitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Localization other = (Localization) obj;
+		if (Double.doubleToLongBits(latitude) != Double.doubleToLongBits(other.latitude))
+			return false;
+		if (Double.doubleToLongBits(longitude) != Double.doubleToLongBits(other.longitude))
+			return false;
+		return true;
+	}
+	
+	public double distanceTo(Localization loc) {
+		double lat = this.latitude - loc.getLatitude();
+		double lon = this.longitude - loc.getLongitude();
+		return Math.sqrt(lat * lat + lon * lon);
+	}
+}
