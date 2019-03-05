592072b5d05b49999d5443d875d6b27787b13f89
Samuel Rincé <samuel.rince.sr@gmail.com>
2019-03-05 14:49:21 +0100

    UserTest and DurationTest file creation

---
+++ src/fr/ecp/IS1220/beribu/DurationTest.java
@@ -0,0 +1,14 @@
+package fr.ecp.IS1220.beribu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DurationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
