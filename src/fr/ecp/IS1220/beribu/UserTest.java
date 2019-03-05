df62df255f12150936784daf26b5ee74aa6ae39c
Samuel Rincé <samuel.rince.sr@gmail.com>
2019-03-05 14:43:59 +0100

    file creation

---
+++ src/fr/ecp/IS1220/beribu/UserTest.java
@@ -0,0 +1,14 @@
+package fr.ecp.IS1220.beribu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
