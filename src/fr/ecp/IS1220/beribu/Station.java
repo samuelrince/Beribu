057abd66aab2f8b2ea69f28f3a7d005f3ee023bf
Samuel Rincé <samuel.rince.sr@gmail.com>
2019-03-10 19:31:19 +0100

    Write unit tests and javadoc for Station class

--- src/fr/ecp/IS1220/beribu/Station.java
+++ src/fr/ecp/IS1220/beribu/Station.java
@@ -31,6 +31,12 @@ public class Station {
 		return this.history;
 	}
 	
+	/**
+	 * This method is used to count the number of Bicycles available in the
+	 * station. To be available, a bicycle has to be parked in a parking slot 
+	 * that is not offline or out of service.
+	 * @return 	The number of bicycles available in the station
+	 */
 	public int numberOfBicycles() {
 		int number = 0;
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
@@ -42,18 +48,32 @@ public class Station {
 		return number;
 	}
 	
+	/**
+	 * This method is used to count the number of Bicycles available in the
+	 * station of certain type defined by bicycleType. To be available, a 
+	 * bicycle has to be parked in a parking slot that is not offline or out
+	 * of service.
+	 * @param bicycleType	A string for the type of bicycle
+	 * @return				The number of bicycle available of the defined type
+	 */
 	public int numberOfBicycles(String bicycleType) {
 		int number = 0;
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (this.parkingSlots.get(i).isBicycle() 
 			&& this.parkingSlots.get(i).isOffline() == false) {
-				if (this.parkingSlots.get(i).getBicycle().getType() == bicycleType)
+				if (bicycleType.equalsIgnoreCase(this.parkingSlots.get(i).getBicycle().getType()))
 					number++;
 			}
 		}
 		return number;
 	}
 	
+	/**
+	 * This method is used to count the number of free parking slots. A free 
+	 * parking slot is counted only if it has not bicycle attached to and if
+	 * it is online.
+	 * @return
+	 */
 	public int numberOfFreeSlots() {
 		int number = 0;
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
@@ -65,6 +85,14 @@ public class Station {
 		return number;
 	}
 	
+	/**
+	 * This method is user to get a free parking slot. The parking slot has
+	 * to be online and has no bicycle attached to. This method return the first 
+	 * available parking slot and ignore the next ones. This function throw a 
+	 * RuntimeException if there is no parking slot available.
+	 * @return						A free ParkingSlot
+	 * @throws RuntimeException		If no ParkingSlot are available
+	 */
 	public ParkingSlot getFreeParkingSlot() throws RuntimeException {
 		for(int i = 0; i <= this.parkingSlots.size() - 1; i++) {
 			if (!this.parkingSlots.get(i).isBicycle() && this.parkingSlots.get(i).isOffline() == false) {
@@ -74,6 +102,13 @@ public class Station {
 		throw new RuntimeException("No parking slot available in this station");
 	}
 	
+	/**
+	 * This method is used to detect if the station is full or not. A station
+	 * is considered as full if there is no parking slot available. That means
+	 * all parking slots has a bicycle attached to or are offline.
+	 * @return		Boolean, true if the station has at least one free parking slot,
+	 * false if not.
+	 */
 	public boolean isFull() {
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (!parkingSlots.get(i).isBicycle()
@@ -84,7 +119,14 @@ public class Station {
 		return false;
 	}
 	
-	public Bicycle getBicycle() {
+	/**
+	 * This method allows to a user to detach a bicycle from the station. In this
+	 * case it is the first available bicycle found. The bicycle type is not
+	 * considered.
+	 * @return		A Bicycle
+	 * @throws 		If no available bicycle found
+	 */
+	public Bicycle getBicycle() throws RuntimeException {
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (this.parkingSlots.get(i).isBicycle() 
 			&& this.parkingSlots.get(i).isOffline() == false) {
@@ -97,7 +139,14 @@ public class Station {
 		throw new RuntimeException("Sorry, no bicycle is available.");
 	}
 		
-	public Bicycle getBicycle(String bicycleType) {
+	/**
+	 * This method allows to a user to detach a bicycle of certain type. The first
+	 * bicycle found that match the bicycle type is detached.
+	 * @param bicycleType			A string that gives the type of the bicycle wanted.
+	 * @return						Return a bicycle with the good type
+	 * @throws RuntimeException		Throw an RuntimeException if no bicycle is found.
+	 */
+	public Bicycle getBicycle(String bicycleType) throws RuntimeException {
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (this.parkingSlots.get(i).isBicycle() 
 			&& this.parkingSlots.get(i).isOffline() == false) {
@@ -112,6 +161,10 @@ public class Station {
 		throw new RuntimeException("Sorry, no bicycle of the wanted type is available");
 	}
 	
+	/**
+	 * This method is used to check if there is a bicycle available
+	 * @return		True is there is a bicycle available and false otherwise
+	 */
 	public boolean isBicycle() {
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (this.parkingSlots.get(i).isBicycle() 
@@ -122,11 +175,15 @@ public class Station {
 		return false;
 	}
 	
+	/**
+	 * This method is used to check if there is a bicycle of a certain type is available
+	 * @return		True is there is a bicycle available and false otherwise
+	 */
 	public boolean isBicycle(String bicycleType) {
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			if (this.parkingSlots.get(i).isBicycle()
 			&& this.parkingSlots.get(i).isOffline() == false) {
-				if (this.parkingSlots.get(i).getBicycle().getType() == bicycleType) {
+				if (bicycleType.equalsIgnoreCase(this.parkingSlots.get(i).getBicycle().getType())) {
 					return true;
 				}
 			}
@@ -153,9 +210,15 @@ public class Station {
 	public Boolean isOffline() {
 		return isOffline;
 	}
-
+	
+	/**
+	 * This method is used to set Offline the station and all parking
+	 * slots.
+	 * @param isOffline
+	 */
 	public void setOffline(Boolean isOffline) {
 		this.isOffline = isOffline;
+		// Set all parking slots offline
 		for (int i = 0; i <= this.parkingSlots.size()-1; i++) {
 			this.parkingSlots.get(i).setOffline(isOffline);
 		}
@@ -164,7 +227,12 @@ public class Station {
 	public ArrayList<ParkingSlot> getParkingSlots() {
 		return parkingSlots;
 	}
-
+	
+	/**
+	 * This method add ParkingSlot to the station. <br> <br>
+	 * <b>This method should be called by a ParkingSlot object only</b>
+	 * @param parkingSlot
+	 */
 	public void addParkingSlot(ParkingSlot parkingSlot) {
 		this.parkingSlots.add(parkingSlot);
 	}
@@ -177,6 +245,11 @@ public class Station {
 		this.targetOf = targetOf;
 	}
  
+	/**
+	 * This method is used to update the status of the station and notify
+	 * the Travels that are liked to. It also add all states into the history
+	 * attribute.
+	 */
 	public void updateStatus() {
 		for (int i = 0; i < this.targetOf.size(); i++) {
 			if (this.isFull()) {
@@ -186,6 +259,14 @@ public class Station {
 		this.history.add(new State());
 	}
 	
+	/**
+	 * A nested class State to provide states feature for stations.
+	 * A State is a dated photography of all parking slots status of 
+	 * the station. The status of parking slot is the combination of
+	 * two booleans representing is the parking isOffline and if it
+	 * has a Bicycle attached to. <br>
+	 * States are stored in a station attribute.
+	 */
 	public class State{
 		private Date timeStamp;
 		private ArrayList<Map.Entry<Boolean, Boolean>> parkingSlotStatus 
--- src/fr/ecp/IS1220/beribu/StationTest.java
+++ src/fr/ecp/IS1220/beribu/StationTest.java
@@ -22,4 +22,525 @@ class StationTest {
 		});
 	}
 	
+	/*
+	 * Test number of bicycle method
+	 */
+	@Test
+	void numberOfBicycleTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfBicycles() == 3);
+	}
+	@Test
+	void numberOfBicycleTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfBicycles("Electrical") == 2);
+	}
+	@Test
+	void numberOfBicycleTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfBicycles("Mechanical") == 1);
+	}
+	@Test
+	void numberOfBicycleTest004() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertTrue(s.numberOfBicycles() == 0);
+	}
+	@Test
+	void numberOfBicycleTest005() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertTrue(s.numberOfBicycles("Mechanical") == 0);
+	}
+	@Test
+	void numberOfBicycleTest006() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertTrue(s.numberOfBicycles("Electrical") == 0);
+	}
+	@Test
+	void numberOfBicycleTest007() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		assertTrue(s.numberOfBicycles("Mechanical") == 0);
+	}
+	@Test
+	void numberOfBicycleTest008() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(3).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfBicycles("Electrical") == 0);
+	}
+	
+	/*
+	 * Test number of free slots methods
+	 */
+	@Test
+	void numberOfFreeSlotsTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfFreeSlots() == 1);
+	}
+	@Test
+	void numberOfFreeSlotsTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfFreeSlots() == 2);
+	}
+	@Test
+	void numberOfFreeSlotsTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertTrue(s.numberOfFreeSlots() == 4);
+	}
+	@Test
+	void numberOfFreeSlotsTest004() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(3).setBicycle(new MechanicalBike());
+		assertTrue(s.numberOfFreeSlots() == 0);
+	}
+	
+	/*
+	 * Test get free parking slot method
+	 */
+	@Test
+	void getFreeParkingSlotTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		ParkingSlot p1 = new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(2).setBicycle(new MechanicalBike());
+		assertEquals(p1, s.getFreeParkingSlot());
+	}
+	@Test
+	void getFreeParkingSlotTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		ParkingSlot p1 = new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertEquals(p1, s.getFreeParkingSlot());
+	}
+	@Test
+	void getFreeParkingSlotTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(3).setBicycle(new MechanicalBike());
+		assertThrows(RuntimeException.class, () -> {
+			s.getFreeParkingSlot();
+		});
+	}
+	
+	/*
+	 * Test is full method
+	 */
+	@Test
+	void isFullTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(3).setBicycle(new MechanicalBike());
+		assertTrue(s.isFull() == false);
+	}
+	@Test
+	void isFullTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		assertTrue(s.isFull() == true);
+	}
+	
+	/*
+	 * Test get bicycle method
+	 */
+	@Test
+	void getBicycleTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new ElectricalBike();
+		s.getParkingSlots().get(0).setBicycle(b);
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		assertEquals(s.getBicycle(), b);
+	}
+	@Test
+	void getBicycleTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new MechanicalBike();
+		s.getParkingSlots().get(0).setBicycle(b);
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		assertEquals(s.getBicycle(), b);
+	}
+	@Test
+	void getBicycleTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new MechanicalBike();
+		s.getParkingSlots().get(0).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(1).setBicycle(b);
+		s.getParkingSlots().get(2).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		assertEquals(s.getBicycle(), b);
+	}
+	@Test
+	void getBicycleTest004() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertThrows(RuntimeException.class, () -> {
+			s.getBicycle();
+		});
+	}
+	@Test
+	void getBicycleTest005() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		assertThrows(RuntimeException.class, () -> {
+			s.getBicycle();
+		});
+	}
+	@Test
+	void getBicycleTest006() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new ElectricalBike();
+		s.getParkingSlots().get(0).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(1).setBicycle(b);
+		assertEquals(b, s.getBicycle("Electrical"));
+	}
+	@Test
+	void getBicycleTest007() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new MechanicalBike();
+		s.getParkingSlots().get(1).setBicycle(b);
+		assertEquals(b, s.getBicycle("Mechanical"));
+	}
+	@Test
+	void getBicycleTest008() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		Bicycle b = new MechanicalBike();
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(b);
+		s.getParkingSlots().get(1).setOffline(true);
+		assertThrows(RuntimeException.class, () -> {
+			s.getBicycle("Mechanical");
+		});
+	}
+	@Test
+	void getBicycleTest010() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(1).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		s.getParkingSlots().get(1).setOffline(true);
+		assertThrows(RuntimeException.class, () -> {
+			s.getBicycle("Eletrical");
+		});
+	}
+	
+	/*
+	 * Test is bicycle method
+	 */
+	@Test
+	void isBicycleTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		assertFalse(s.isBicycle());
+	}
+	@Test
+	void isBicycleTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		assertTrue(s.isBicycle());
+	}
+	@Test
+	void isBicycleTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new MechanicalBike());
+		assertTrue(s.isBicycle());
+	}
+	@Test
+	void isBicycleTest004() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		assertFalse(s.isBicycle());
+	}
+	@Test
+	void isBicycleTest005() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new MechanicalBike());
+		s.getParkingSlots().get(1).setBicycle(new MechanicalBike());
+		assertTrue(s.isBicycle("Mechanical"));
+	}
+	@Test
+	void isBicycleTest006() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		assertFalse(s.isBicycle("Mechanical"));
+	}
+	@Test
+	void isBicycleTest007() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		assertFalse(s.isBicycle("Electrical"));
+	}
+	
+	/*
+	 * Test set offline
+	 */
+	@Test
+	void setOfflineTest001() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.getParkingSlots().get(0).setBicycle(new ElectricalBike());
+		s.getParkingSlots().get(0).setOffline(true);
+		s.setOffline(true);
+		assertTrue(s.getParkingSlots().get(0).isOffline() && s.getParkingSlots().get(1).isOffline());
+	}
+	@Test
+	void setOfflineTest002() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		new ParkingSlot(s);
+		new ParkingSlot(s);
+		s.setOffline(true);
+		assertTrue(s.getParkingSlots().get(0).isOffline() && s.getParkingSlots().get(1).isOffline());
+	}
+	@Test
+	void setOfflineTest003() {
+		SystemDate SD = SystemDate.getInstance();
+		SD.setDay(2019, 12, 12);
+		SD.setTime(5, 34, 45);
+		Station s = new Station(new Localization(0.8, 0.7), false);
+		assertDoesNotThrow(() -> {
+			s.setOffline(true);
+		});
+	}
 }
