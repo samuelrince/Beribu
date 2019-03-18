4071f73caa5eb0453b7f3abb45d07438b62c90a5
Samuel Rince <samuel.rince.sr@gmail.com>
2019-03-18 21:44:30 +0100

    Remove mian method

--- src/fr/ecp/IS1220/beribu/Bicycle.java
+++ src/fr/ecp/IS1220/beribu/Bicycle.java
@@ -36,14 +36,4 @@ public abstract class Bicycle {
 		typeDict.add("ELECTRICAL");	
 		return typeDict;
 	}
-	
-	public static void main(String[] args) {
-		ElectricalBike eBike1 = new ElectricalBike();
-		MechanicalBike mBike1 = new MechanicalBike();
-		ElectricalBike eBike2 = new ElectricalBike();
-		System.out.println(eBike1.getType()+" "+eBike1.getId());
-		System.out.println(mBike1.getType()+" "+mBike1.getId());
-		System.out.println(eBike2.getType()+" "+eBike2.getId());
-	}
-	
 }
