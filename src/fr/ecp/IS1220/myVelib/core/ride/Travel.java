214575bba9bdb6cfe448ef67e8da31e1180988be
Vazroc <valvierge@gmail.com>
2019-04-04 18:52:41 +0200

    Correction d'un bug lié aux trajets planifiés dont le type de vélo n'est pas précisé

--- src/fr/ecp/IS1220/myVelib/core/ride/Travel.java
+++ src/fr/ecp/IS1220/myVelib/core/ride/Travel.java
@@ -284,8 +284,8 @@ public class Travel implements java.io.Serializable {
 				this.suggestedEndStation = this.destination.getClosestAvailableStation();
 				this.suggestedEndStation.addTargetOf(this);
 				this.previsionDuration = new Duration(this.suggestedStartStation,
-						this.suggestedEndStation, this.bicycleType);
-				this.previsionCost = this.user.getCard().cost(this.previsionDuration,this.bicycleType);
+						this.suggestedEndStation, this.pathStrategy.getBicycleType());
+				this.previsionCost = this.user.getCard().cost(this.previsionDuration,this.pathStrategy.getBicycleType());
 				this.previsionDuration.add(new Duration(this.source,this.suggestedStartStation.getLocalization(),4));
 				this.previsionDuration.add(new Duration(this.destination,this.suggestedEndStation.getLocalization(),4));
 			}
